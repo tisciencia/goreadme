6e5b6e7dbe1315fed3198dc1e861fee594d1aca2
vintem <0rlm@g1c>
2013-07-24 22:29:13 -0400

    removed files

--- server/config/google-settings.js
+++
@@ -1,3 +0,0 @@
-exports.GOOGLE_CLIENT_ID = "241355542157.apps.googleusercontent.com";
-exports.GOOGLE_CLIENT_SECRET = "Z-TL2zhvPuV4AAJn7H7bNGu_";
-exports.GOOGLE_CALLBACK = "http://localhost:3000/auth/google/callback";
