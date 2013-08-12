1e6283e798dfb183df9abb2af9696ac9edac744e
Thiago Temple <vintem@gmail.com>
2013-08-12 09:58:25 -0400

    rename local database

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   root: require('path').normalize(__dirname + '/..'),
   app: {
-    name: 'go reader'
+    name: 'go-read me'
   },
   port: process.env.PORT || 3000,
-  db: process.env.MONGO_URL || 'mongodb://localhost/goreader'
+  db: process.env.MONGO_URL || 'mongodb://localhost/goreadme'
 };
