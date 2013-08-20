8f247ccce538c1bee8684d2b6b345185fdf4d2c8
vintem <0rlm@g1c>
2013-08-19 23:06:51 -0400

    refactored encodeUri filter

---
+++ app/scripts/controllers/Main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('goreadmeApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -5,9 +5,9 @@ basePath = '';
 
 // list of files / patterns to load in the browser
 files = [
-	ANGULAR_SCENARIO,
-	ANGULAR_SCENARIO_ADAPTER,
-	'test/e2e/**/*.js'
+  ANGULAR_SCENARIO,
+  ANGULAR_SCENARIO_ADAPTER,
+  'test/e2e/**/*.js'
 ];
 
 // list of files to exclude
@@ -49,3 +49,10 @@ captureTimeout = 5000;
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
 singleRun = false;
+
+// Uncomment the following lines if you are using grunt's server to run the tests
+// proxies = {
+//   '/': 'http://localhost:9000/'
+// };
+// URL root prevent conflicts with the site root
+// urlRoot = '_karma_';
